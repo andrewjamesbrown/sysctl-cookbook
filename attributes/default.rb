2851b00e3c8f245127cb8f08a2ecccf29863b931
Andrew Brown <andrew.brown@shopify.com>
2016-12-07 14:07:51 -0500

    Use node attributes as hash

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,6 @@ default[:sysctl] = {}
 default[:sysctl][:values] = {}
 
 default[:sysctl][:config_file] = '/etc/sysctl.conf'
-if platform_family == 'debian'
+if node['platform_family'] == 'debian'
   default[:sysctl][:config_file] = '/etc/sysctl.d/99-chef.conf'
 end
